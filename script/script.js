bf68c05e7ac9a634109e0984a0ac1eb1cff42111
Mikita Ivanou <nikitaivanou.gmail.com>
2020-01-24 02:09:22 +0300

    Первый коммит

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let money = 10;
+
+let income = 10;
+
+let addExpences = 10;
+
+let deposit = 10;
+
+let mission = 10;
+
+let period = 10;
+
+alert("Hi! What a wonderful day!");
+
+console.log("Hi! It's a message for console")
