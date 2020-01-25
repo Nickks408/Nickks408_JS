d6b539a42ddffa10724782dcbac007450121a577
Mikita Ivanou <nikitaivanou.gmail.com>
2020-01-26 01:06:20 +0300

    initial

--- script/script.js
+++ script/script.js
@@ -1,15 +1,20 @@
-let money = 10;
+let money = 1000;
+console.log(typeof money);
+let income = 'Фриланс, банковский вклад';
+console.log(typeof income);
+let addExpences = 'Интернет, такси, коммуналка, корм для хомячка';
+console.log(addExpences.length)
+let deposit = true;
+console.log(typeof deposit);
+let mission = 1000000;
 
-let income = 10;
+let period = 12;
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей');
+console.log(addExpences.toLowerCase());
+console.log(addExpences.split(', '));
+//alert("Hi! What a wonderful day!");
 
-let addExpences = 10;
-
-let deposit = 10;
-
-let mission = 10;
-
-let period = 10;
-
-alert("Hi! What a wonderful day!");
-
-console.log("Hi! It's a message for console")
+let budgetDay = (money / 30);
+console.log(budgetDay);
+//console.log("Hi! It's a message for console")
