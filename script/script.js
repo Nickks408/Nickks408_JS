63f0eafbfdf0507ae6403a9dc619ea17de867c9e
Mikita Ivanou <nikitaivanou.gmail.com>
2020-01-28 14:43:16 +0300

    Added new code

--- script/script.js
+++ script/script.js
@@ -2,19 +2,47 @@ let money = 1000;
 console.log(typeof money);
 let income = 'Фриланс, банковский вклад';
 console.log(typeof income);
-let addExpences = 'Интернет, такси, коммуналка, корм для хомячка';
-console.log(addExpences.length)
-let deposit = true;
-console.log(typeof deposit);
-let mission = 1000000;
+
+
+let mission = 100;
 
 let period = 12;
 console.log('Период равен', period, 'месяцев');
 console.log('Цель заработать', mission, 'рублей');
-console.log(addExpences.toLowerCase());
-console.log(addExpences.split(', '));
-//alert("Hi! What a wonderful day!");
 
-let budgetDay = (money / 30);
-console.log(budgetDay);
-//console.log("Hi! It's a message for console")
+
+let monthIncome = +prompt('Ваш месячный доход');
+console.log(typeof String(monthIncome));
+
+let addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log(typeof (addExpences));
+
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(typeof deposit);
+
+let expences1 = prompt('Введите обязательную статью расходов');
+let amount1 = +prompt('Во сколько это обойдется');
+let expences2 = prompt('Введите обязательную статью расходов');
+let amount2 = +prompt('Во сколько это обойдется');
+
+let budgetMonth = (monthIncome - (amount1 + amount2));
+console.log('Бюджет на месяц ' + budgetMonth);
+
+let periodTarget = (mission / budgetMonth);
+console.log('Цель будет достигнута за ' + Math.round(periodTarget) + ' месяцев');
+
+let budgetDay = (budgetMonth / 30);
+console.log('Бюджет на день: ' + budgetDay);
+
+
+if (budgetDay >= 1200){ 
+    console.log ('У вас высокий уровень дохода');
+} else if(budgetDay >=600 && (budgetDay <1200)){
+    console.log ('У вас средний уровень дохода');
+} else if(budgetDay <600 && (budgetDay >0)){
+    console.log ('К сожалению, у вас низкий уровень дохода');
+}
+else {
+    console.log ('Что-то пошло не так!');
+}
+   
